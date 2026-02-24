383808240a00621145e28b6dc8d3830d9944b8ff
profpatrickoli <138583814+profpatrickoli@users.noreply.github.com>
2026-02-24 09:45:10 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,10 @@
+// Tipagem de dados no JS
+// Tipagem dinâmica: o próprio interpretador atribui o tipo do dado da variável
+nome = "Patrick"
+idade = 16
+altura = 1.70
+estuda = true
+console.log(typeof(nome))
+console.log(typeof(idade))
+console.log(typeof(altura))
+console.log(typeof(estuda))
---
+++ aula2.js
@@ -0,0 +1,16 @@
+// const = variável constante, o valor armazenado não pode ser alterado
+// let = variável local
+// var = variável global
+// Calculadora em JS
+const v1 = 1500
+const v2 = "2"
+
+const soma = v1 + Number(v2)
+const sub = v1 - v2
+const mul = v1 * v2
+const div = v1 / v2
+
+console.log("A soma é: " + soma)
+console.log("A subtração é: " + sub)
+console.log("A multiplicação é: " + mul)
+console.log("A divisão é: " + div)
