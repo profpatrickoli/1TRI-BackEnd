8df32e0ebe787796927ad1ba2d2f4b8e5e97b900
profpatrickoli <138583814+profpatrickoli@users.noreply.github.com>
2026-03-17 11:53:10 -0300

    Add files via upload

---
+++ aula7ex2.js
@@ -0,0 +1,25 @@
+/*
+Exercício 2 (vale nota)
+CRIAR UM "BANCO DE DADOS" DE ALGUMA DAS SEGUINTES CATEGORIAS:
+LIVROS, FILMES, SÉRIES, JOGOS, ANIMES.
+USANDO OBJETOS, COM PELO MENOS 10 ELEMENTOS.
+
+SEGUIR O MODELO ABAIXO:
+*/
+const modelo = {
+    titulo: "Tìtulo do Livro",
+    autor: "Autor",
+    ano: 9999,
+    paginas: 333,
+    sinopse: "Quando virou o envelope, com a mão trêmula, Harry viu um lacre de cera púrpura com um brasão; um leão, uma águia, um texugo e uma cobra circulando uma grande letra\nHarry Potter nunca havia ouvido falar de Hogwarts quando as cartas começaram a aparecer no capacho da Rua dos Alfeneiros, nº 4. Escritos a tinta verde-esmeralda em pergaminho amarelado com um lacre de cera púrpura, as cartas eram rapidamente confiscadas por seus pavorosos tio e tia. Então, no aniversário de onze anos de Harry, um gigante com olhos que luziam como besouros negros chamado Rúbeo Hagrid surge com notícias surpreendentes: Harry Potter é um bruxo e tem uma vaga na Escola de Magia e Bruxaria de Hogwarts. Uma incrível aventura está para começar!",
+    preco: 99.90
+}
+
+const livros = [
+    {
+        // livro 1
+    },
+    {
+        // livro2 
+    }
+]
